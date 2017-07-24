3fa6425f1d5857786815e5e5029e4757e94f04d7
Tim <sarrtim@gmail.com>
2017-07-25 00:23:31 +0300

    rebase

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "calendar_schedular.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "calendar_scheduler.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
