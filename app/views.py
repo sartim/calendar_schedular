6ef87e8592f88c2f948c5e45bb11a98df4014ced
Tim <sarrtim@gmail.com>
2017-07-25 00:07:24 +0300

    init

---
+++ app/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+
+def calendar_schedule(request):
+    context = {}
+    return render(request, 'app/base.html', context)
