e09a9e4f5184adf5e230fa19d6e86f2e385ab92f
Tim <sarrtim@gmail.com>
2017-07-25 23:00:36 +0300

    Add Fullcalendar

--- app/views.py
+++ app/views.py
@@ -6,4 +6,4 @@ from django.shortcuts import render
 
 def calendar_schedule(request):
     context = {}
-    return render(request, 'app/base.html', context)
+    return render(request, 'app/calendar.html', context)
