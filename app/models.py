71bfe62d17e82d38e0f39c81f5257cee4c49ac50
Tim <sarrtim@gmail.com>
2017-12-20 10:58:51 +0300

    update Event model

--- app/models.py
+++ app/models.py
@@ -7,5 +7,5 @@ from django.db import models
 class Event(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
+    start = models.DateTimeField()
+    end = models.DateTimeField()
