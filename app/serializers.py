5912b3b5ff798d7aaba1fe0055a40abbc4a51ea8
Tim <sarrtim@gmail.com>
2018-01-11 17:07:32 +0300

    change serializer to ModelSerializer

--- app/serializers.py
+++ app/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
+from .models import Event
 
 
-class EventSerializer(serializers.Serializer):
+class EventSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField()
     description = serializers.CharField()
     start = serializers.CharField()
     end = serializers.CharField()
+
+    class Meta:
+        model = Event
+        fields = ('id', 'title', 'description', 'start', 'end',)
